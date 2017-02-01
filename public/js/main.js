240e2520b1490e87c659874193c796c552175628
Florin Pop <popflorin1705@yahoo.com>
2017-02-02 01:36:14 +0200

    move and edges user

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,41 @@ class User {
         this.x = width/2;
         this.y = width/2;
         this.r = 30;
+        this.speed = 5;
+    }
+    
+    draw(){
+        fill(200, 0, 100);
+        ellipse(this.x, this.y, this.r*2, this.r*2);
+    }
+    
+    move(){
+    if (keyIsDown(LEFT_ARROW))
+        this.x -= this.speed;
+
+    if (keyIsDown(RIGHT_ARROW))
+        this.x += this.speed;
+
+    if (keyIsDown(UP_ARROW))
+        this.y -= this.speed;
+
+    if (keyIsDown(DOWN_ARROW))
+        this.y += this.speed;
+        
+    }
+    
+    edges() {
+        if (this.x > width + this.r) {
+            this.x = -this.r;
+        } else if (user.x < -this.r) {
+            this.x = width + this.r;
+        }
+
+        if (this.y > height + this.r) {
+            this.y = -this.r;
+        } else if (this.y < -this.r) {
+            this.y = height + this.r;
+        }
     }
 }
 
@@ -17,6 +52,7 @@ function setup(){
 function draw() {
     background(0);
     
-    fill(200, 0, 100);
-    ellipse(user.x, user.y, user.r*2, user.r*2);
+    user.move();
+    user.edges();
+    user.draw();
 }
