1ace34fd2b86cb4e40ba729f193d7f986e8d9b0d
Florin Pop <popflorin1705@yahoo.com>
2017-02-02 01:57:49 +0200

    bump from the edges - ball

--- public/js/ball.js
+++ public/js/ball.js
@@ -22,13 +22,17 @@ class Ball {
     edges() {
         if(this.x + this.r > width){
             this.x = width - this.r;
+            this.velX *= -1;
         } else if (this.x - this.r < 0) {
+            this.velX *= -1;
             this.x = this.r;
         }
         
         if(this.y + this.r > height){
+            this.velY *= -1;
             this.y = height - this.r;
         } else if (this.y - this.r < 0) {
+            this.velY *= -1;
             this.y = this.r;
         }
     }
