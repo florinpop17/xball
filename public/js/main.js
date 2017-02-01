451b676347c25085533685c4752d3d48aeb6dbe3
Florin Pop <popflorin1705@yahoo.com>
2017-02-02 01:32:28 +0200

    user class + drawing to the canvas

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,22 @@
+class User {
+    constructor(){
+        this.x = width/2;
+        this.y = width/2;
+        this.r = 30;
+    }
+}
+
+let user;
+
 function setup(){
     createCanvas(800, 800);
+    
+    user = new User;
 }
 
 function draw() {
     background(0);
+    
+    fill(255);
+    ellipse(user.x, user.y, user.r*2, user.r*2);
 }
