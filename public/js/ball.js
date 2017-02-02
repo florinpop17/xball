2b33bd34090b432982a41eb99c41919c527fe4c5
Florin Pop <popflorin1705@yahoo.com>
2017-02-02 13:35:45 +0200

    velocity and location fix

--- public/js/ball.js
+++ public/js/ball.js
@@ -2,6 +2,7 @@ class Ball {
     constructor() {
         this.location = createVector(random(width), random(height));
         this.velocity = createVector(0, 0);
+        this.acceleration = createVector(0, 1);
         this.r = 15;
     }
     
@@ -13,24 +14,24 @@ class Ball {
     draw() {
         fill(255);
         strokeWeight(3);
-        ellipse(this.x, this.y, this.r*2, this.r*2);
+        ellipse(this.location.x, this.location.y, this.r*2, this.r*2);
     }
     
     edges() {
-        if(this.x + this.r > width){
-            this.x = width - this.r;
-            this.velX *= -1;
-        } else if (this.x - this.r < 0) {
-            this.velX *= -1;
-            this.x = this.r;
+        if(this.location.x + this.r > width){
+            this.location.x = width - this.r;
+            this.velocity.x *= -1;
+        } else if (this.location.x - this.r < 0) {
+            this.velocity.x *= -1;
+            this.location.x = this.r;
         }
         
-        if(this.y + this.r > height){
-            this.velY *= -1;
-            this.y = height - this.r;
-        } else if (this.y - this.r < 0) {
-            this.velY *= -1;
-            this.y = this.r;
+        if(this.location.y + this.r > height){
+            this.velocity.y *= -1;
+            this.location.y = height - this.r;
+        } else if (this.location.y - this.r < 0) {
+            this.velocity.y *= -1;
+            this.location.y = this.r;
         }
     }
 }
